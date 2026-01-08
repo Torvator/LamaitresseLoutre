58291a41be1f691f8e96cd8b66dca2e40c61f14f
Torvator <pro.alexis.costa@gmail.com>
2026-01-08 02:02:58 +0100

    Sécurisation et corrections logon logoutV8

---
+++ src/theme/DocRoot/index.js
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react';
+import DocRoot from '@theme-original/DocRoot';
+import { useAuth } from '../../utils/useAuth';
+import { redirectTo, ROUTES } from '../../utils/routes';
+import Layout from '@theme/Layout';
+
+export default function DocRootWrapper(props) {
+  const { user, loading } = useAuth();
+
+  useEffect(() => {
+    if (!loading && !user) {
+      redirectTo(ROUTES.LOGIN);
+    }
+  }, [user, loading]);
+
+  if (loading) {
+    return (
+      <Layout>
+        <div style={{
+          maxWidth: '600px',
+          margin: '4rem auto',
+          padding: '0 2rem',
+          textAlign: 'center',
+        }}>
+          <p style={{ fontSize: '1.2rem', color: '#4a4a4a' }}>Chargement...</p>
+        </div>
+      </Layout>
+    );
+  }
+
+  if (!user) {
+    return (
+      <Layout>
+        <div style={{
+          maxWidth: '600px',
+          margin: '4rem auto',
+          padding: '0 2rem',
+          textAlign: 'center',
+        }}>
+          <p style={{ fontSize: '1.2rem', color: '#4a4a4a' }}>Redirection vers la connexion...</p>
+        </div>
+      </Layout>
+    );
+  }
+
+  return <DocRoot {...props} />;
+}
